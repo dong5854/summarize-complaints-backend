f114f29766264e93b1520ea588859858667c9e50
dongyoung <dong5854@gmail.com>
2022-07-04 18:27:38 +0900

    feat: 파일 저장 위치ConfigurationProperties 추가

--- src/main/java/com/summarizer/complaints/ComplaintsApplication.java
+++ src/main/java/com/summarizer/complaints/ComplaintsApplication.java
@@ -1,13 +1,17 @@
 package com.summarizer.complaints;
 
+import com.summarizer.complaints.config.StorageProperties;
+import com.summarizer.complaints.domain.fileupload.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class ComplaintsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ComplaintsApplication.class, args);
 	}
-
 }
