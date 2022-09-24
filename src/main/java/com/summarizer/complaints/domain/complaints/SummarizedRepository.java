eb7a1f6c792da8a825febb7256ee2ef2c84dcd0a
dong young <dong5854@gmail.com>
2022-09-24 13:47:55 +0900

    feat: add methods to summarizedRepository

--- src/main/java/com/summarizer/complaints/domain/complaints/SummarizedRepository.java
+++ src/main/java/com/summarizer/complaints/domain/complaints/SummarizedRepository.java
@@ -3,7 +3,15 @@ package com.summarizer.complaints.domain.complaints;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SummarizedRepository extends JpaRepository<SummarizedEntity, String> {
-    SummarizedEntity findByUsername(String username);
+    SummarizedEntity findSummarizedEntityById(String id);
+    SummarizedEntity findSummarizedEntityByComplaintId(String complaintId);
+    SummarizedEntity findSummarizedEntityByOriginalTextId(String originalTextId);
+    SummarizedEntity findSummarizedEntityByOriginalVoiceId(String originalVoiceId);
+
+    List<SummarizedEntity> findSummarizedEntitiesByUsername(String username);
+
 }
