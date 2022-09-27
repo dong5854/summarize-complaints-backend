30362b1d25b44fc5366b9ca01abaa8fc1b11fe65
dong young <dong5854@gmail.com>
2022-09-27 14:59:09 +0900

    refactor: edit complaint service, controller

--- src/main/java/com/summarizer/complaints/service/complaints/ComplaintService.java
+++ src/main/java/com/summarizer/complaints/service/complaints/ComplaintService.java
@@ -2,7 +2,9 @@ package com.summarizer.complaints.service.complaints;
 
 import com.summarizer.complaints.domain.complaints.ComplaintEntity;
 import com.summarizer.complaints.domain.complaints.ComplaintRepository;
+import com.summarizer.complaints.web.complaints.dto.ComplaintGetResponseDTO;
 import com.summarizer.complaints.web.complaints.dto.ComplaintPostRequestDTO;
+import com.summarizer.complaints.web.complaints.dto.ComplaintPostResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,15 +16,16 @@ public class ComplaintService {
     private final ComplaintRepository complaintRepository;
 
     @Transactional
-    public ComplaintEntity postComplaintService(ComplaintPostRequestDTO complaintPostRequestDTO){
+    public ComplaintPostResponseDTO postComplaintService(ComplaintPostRequestDTO complaintPostRequestDTO){
             ComplaintEntity complaintEntity = ComplaintEntity.builder()
                     .name(complaintPostRequestDTO.getName())
                     .blackListed(complaintPostRequestDTO.isBlackListed())
                     .build();
-            return complaintEntity;
+        return new ComplaintPostResponseDTO(complaintEntity.getId(), complaintEntity.getName(), complaintEntity.isBlackListed());
     }
 
-    public ComplaintEntity getComplaintService(Long id){
-            return complaintRepository.findComplaintEntityById(id);
+    public ComplaintGetResponseDTO getComplaintService(Long id){
+            ComplaintEntity complaintEntity =  complaintRepository.findComplaintEntityById(id);
+        return new ComplaintGetResponseDTO(complaintEntity.getId(), complaintEntity.getName(), complaintEntity.isBlackListed());
     }
 }
--- src/main/java/com/summarizer/complaints/web/complaints/ComplaintsController.java
+++ src/main/java/com/summarizer/complaints/web/complaints/ComplaintsController.java
@@ -1,10 +1,6 @@
 package com.summarizer.complaints.web.complaints;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.summarizer.complaints.domain.complaints.ComplaintEntity;
-import com.summarizer.complaints.domain.complaints.OriginalEntity;
-import com.summarizer.complaints.domain.complaints.SummarizedEntity;
 import com.summarizer.complaints.domain.complaints.VoiceEntity;
 import com.summarizer.complaints.service.complaints.ComplaintService;
 import com.summarizer.complaints.service.complaints.OriginalService;
@@ -12,7 +8,6 @@ import com.summarizer.complaints.service.complaints.SummarizedService;
 import com.summarizer.complaints.service.complaints.VoiceService;
 import com.summarizer.complaints.web.complaints.dto.*;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -56,26 +51,22 @@ public class ComplaintsController {
     }
 
     @PostMapping(value= "/original") // 원본 파일 업로드
-    public OriginalPostResponseDTO postOriginal(@RequestBody OriginalPostRequestDTO originalPostRequestDTO) throws JsonProcessingException {
+    public OriginalPostResponseDTO postOriginal(@RequestBody OriginalPostRequestDTO originalPostRequestDTO) {
         return originalService.postOriginal(originalPostRequestDTO);
     }
 
     @GetMapping(value = "/original/{id}") // 원본 파일 다운로드
-    public OriginalGetResponseDTO getOriginal(@PathVariable Long id) throws JsonProcessingException {
+    public OriginalGetResponseDTO getOriginal(@PathVariable Long id) {
         return originalService.getOriginal(id);
     }
 
     @PostMapping(value = "/complaint") // 민원인 데이터 삽입
-    public String postComplaint(@RequestBody ComplaintPostRequestDTO complaintPostRequestDTO) throws JsonProcessingException {
-        ComplaintEntity complaintEntity = complaintService.postComplaintService(complaintPostRequestDTO);
-        ComplaintPostResponseDTO complaintPostResponseDTO = new ComplaintPostResponseDTO(complaintEntity.getId(), complaintEntity.getName(), complaintEntity.isBlackListed());
-        return mapper.writeValueAsString(complaintPostResponseDTO);
+    public ComplaintPostResponseDTO postComplaint(@RequestBody ComplaintPostRequestDTO complaintPostRequestDTO) {
+        return complaintService.postComplaintService(complaintPostRequestDTO);
     }
 
     @GetMapping(value = "/complaint/{id}") // 민원인 데이터 조회
-    public String getComplaint(@PathVariable Long id) throws JsonProcessingException {
-        ComplaintEntity complaintEntity = complaintService.getComplaintService(id);
-        ComplaintGetResponseDTO complaintGetResponseDTO = new ComplaintGetResponseDTO(complaintEntity.getId(), complaintEntity.getName(), complaintEntity.isBlackListed());
-        return mapper.writeValueAsString(complaintGetResponseDTO);
+    public ComplaintGetResponseDTO getComplaint(@PathVariable Long id) {
+        return complaintService.getComplaintService(id);
     }
 }
